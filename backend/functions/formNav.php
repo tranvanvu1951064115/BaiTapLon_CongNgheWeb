31f55d3d0497c3725a893319eb803e4699b267fb
tranvanvu1951064115 <96403788+tranvanvu1951064115@users.noreply.github.com>
2021-12-26 21:23:01 +0700

    Delete formNav.php

--- backend/functions/formNav.php
+++
@@ -1,7 +0,0 @@
-<nav class="form-nav">
-            <ul>
-                <li><a href="index"><img class="form-logo" src="frontend/assets/image/sub-logo.png" alt="">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Languages:</a></li>
-            </ul>
-</nav>
