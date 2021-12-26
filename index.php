04031147381f0ef96ead89132e580577959f15b7
Linhchi2012 <94341441+Linhchi2012@users.noreply.github.com>
2021-12-26 21:39:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<?php include 'backend/shared/header.php';?>
+    <section class="main-page">
+      <div class="container ">
+        <div class="image ">
+          <svg viewBox="0 0 24 24" aria-hidden="true" class="big-bird"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
+        </div>
+      </div>
+     
+      <div class="content  ">
+        <div class="col-sm-6 mt-3">
+          <div class=><svg xmlns="http://www.w3.org/2000/svg"   fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+          </svg> 
+          <h1 class="mt-4">Happening now</h1>
+          <h2>Join twitter today</h2>
+        </div>
+        <div class="d-grid gap-2 col-6">
+          <a
+            class="btn btn-primary rounded-pill text-muted"
+            type="button"
+            href="#"
+            ><img src="https://img.icons8.com/fluency/48/000000/google-logo.png" style="width: 15px"/>
+            Sign up with Google</a
+          >
+          <a
+            class="btn btn-primary rounded-pill fw-bold"
+            type="button normal"
+            href="#"
+            ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-apple" viewBox="0 0 16 16">
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z"/>
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282z"/>
+            </svg>
+            Sign up with Apple</a
+          >
+        </div>
+      
+        <div class="divider d-flex align-items-center my-4">
+          <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+        </div>
+        
+      <div class="d-grid gap-2 col-6">
+        <a
+          class="sign_up_now btn btn-primary rounded-pill fw-bold"
+          type="button"
+          href="#"
+          >Sign up with phone or email</a
+        >
+      </div>
+      <div class="note gap-2 mt-1" >
+        <p>
+          By signing up, you agree to the 
+          <span>
+              <a href="https://twitter.com/en/tos">Terms of Service</a>
+          </span>
+      and 
+          <span>
+              <a href="https://twitter.com/en/privacy">Privacy Policy,</a>
+          </span>
+      including
+          <span>
+              <a href="https://help.twitter.com/en/rules-and-policies/twitter-cookies">Cookie Use</a>
+          </span>
+        </p>
+         
+      </div>
+      <h1 class="already mt-4 fw-bold">Already have an account?</h1>
+      <a
+      class="signin btn btn-primary rounded-pill fw-bold col-6" style="color: rgb(55, 153, 245);"
+      type="button normal"
+      href="#"
+      >Sign in</a>
+      </div>
+       </div>
+      </div>
+    </div>
+      </div>
+      <div class="footer">
+          <ul>
+            <li><a href="https://about.twitter.com/en">About</a></li>
+            <li><a href="https://help.twitter.com/en">Help Center</a></li>
+            <li><a href="https://twitter.com/en/tos">Terms of Service</a></li>
+            <li><a href="https://twitter.com/en/privacy">Privacy Policy</a></li>
+            <li><a href="https://help.twitter.com/en/rules-and-policies/twitter-cookie">Cookie Policy</a></li>
+            <li><a href="https://help.twitter.com/en/resources/accessibility">Accessibility</a></li>
+            <li><a href="https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=adsinfo">Ads info</a></li>
+            <li><a href="https://blog.twitter.com/">Blog</a></li>
+            <li><a href="https://status.twitterstat.us/">Status</a></li>
+            <li><a href="https://careers.twitter.com/">Careers</a></li>
+            <li><a href="https://about.twitter.com/en/who-we-are/brand-toolkit">Brand Resources</a></li>
+            <li><a href="https://ads.twitter.com/login?ref=gl-tw-tw-twitter-advertise">Advertising</a></li>
+            <li><a href="https://marketing.twitter.com/en">Maketing</a></li>
+            <li><a href="https://business.twitter.com/?ref=web-twc-ao-gbl-twitterforbusiness&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=twitterforbusiness">Twitter for Business</a></li>
+            <li><a href="https://developer.twitter.com/en">Developers</a></li>
+            <li><a href="https://twitter.com/i/directory/profiles">Directory</a></li>
+            <li><a href="https://twitter.com/settings/account/personalization">Settings</a></li>
+          </ul>
+          <p>© 2021 Twitter, Inc.</p>
+        </div>
+      </div>
+    </section>
+      
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+      integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+      integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
