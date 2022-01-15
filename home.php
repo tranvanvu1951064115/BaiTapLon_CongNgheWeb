1657e468026a7b80717472f09481be941c03f024
MrHia <minhquanga01km2@gmail.com>
2022-01-16 04:04:16 +0700

    upload file

--- home.php
+++ home.php
@@ -38,15 +38,15 @@ echo "<script src='./frontend/assets/js/leftSideBar/popUpUserLogout.js' type='mo
 echo "<script src='./frontend/assets/js/home/processDisplayImageWhenUpload.js' defer></script>";
 echo "<script src='./frontend/assets/js/home/app.js' type='module' defer></script>";
 echo "<script src='./frontend/assets/js/home/handleReply.js' defer></script>";
-echo "<script src='./frontend/assets/js/home/navProfile.js' defer></script>";
-echo "<script src='./frontend/assets/js/home/processDistImg.js' defer></script>";
-echo "<script src='./frontend/assets/js/profile/profileSetUpPopUp.js' defer></script>";
-echo "<script src='./backend/ajax/handleTweet.js' type='module' defer></script>";
-echo "<script src='./backend/ajax/handleDelTweet.js' defer></script>";
-echo "<script src='./backend/ajax/handleLoveTweet.js' defer></script>";
-echo "<script src='./backend/ajax/handleComment.js' defer></script>";
-echo "<script src='./backend/ajax/handleDisplayTweet.js' defer></script>";
-echo "<script src='./backend/ajax/handleFollow.js' defer></script>";
+// echo "<script src='./frontend/assets/js/home/navProfile.js' defer></script>";
+// echo "<script src='./frontend/assets/js/home/processDistImg.js' defer></script>";
+// echo "<script src='./frontend/assets/js/profile/profileSetUpPopUp.js' defer></script>";
+// echo "<script src='./backend/ajax/handleTweet.js' type='module' defer></script>";
+// echo "<script src='./backend/ajax/handleDelTweet.js' defer></script>";
+// echo "<script src='./backend/ajax/handleLoveTweet.js' defer></script>";
+// echo "<script src='./backend/ajax/handleComment.js' defer></script>";
+// echo "<script src='./backend/ajax/handleDisplayTweet.js' defer></script>";
+// echo "<script src='./backend/ajax/handleFollow.js' defer></script>";
 ?>
 <div class="wrapper">
     <div class='overlay'></div>
