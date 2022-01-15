598810ff6c501a65f96079449473b1a79c62257f
MrHia <minhquanga01km2@gmail.com>
2022-01-16 05:34:09 +0700

    upload file

--- home.php
+++ home.php
@@ -39,15 +39,8 @@ echo "<script src='./frontend/assets/js/home/processDisplayImageWhenUpload.js' d
 echo "<script src='./frontend/assets/js/home/app.js' type='module' defer></script>";
 // echo "<script src='./frontend/assets/js/home/handleReply.js' defer></script>";
 // echo "<script src='./frontend/assets/js/home/navProfile.js' defer></script>";
-// // echo "<script src='./frontend/assets/js/home/processDistImg.js' defer></script>";
-// // echo "<script src='./frontend/assets/js/profile/profileSetUpPopUp.js' defer></script>";
-// // echo "<script src='./backend/ajax/handleTweet.js' type='module' defer></script>";
-// // echo "<script src='./backend/ajax/handleDelTweet.js' defer></script>";
-// // echo "<script src='./backend/ajax/handleLoveTweet.js' defer></script>";
-// echo "<script src='./backend/ajax/handleComment.js' defer></script>";
-// echo "<script src='./backend/ajax/handleDisplayTweet.js' defer></script>";
-// echo "<script src='./backend/ajax/handleFollow.js' defer></script>";
-// ?>
+git add .
+
 <div class="wrapper">
     <div class='overlay'></div>
     <div class="container">
